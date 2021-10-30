90048b143eff89f78511158e0e1366fb4b98e09f
Илья Долженко <ilya.dolzhenko@kindda.app>
2021-10-30 17:52:34 +0300

    added footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,12 +3,164 @@ import Link from "next/link";
 import clsx from "clsx";
 
 import { Container } from "components/Container/Container";
+import { TextField } from "components/TextField/TextField";
+import { Button } from "components/Button/Button";
+import { ERoute } from "enum/route";
+import { CATEGORIES } from "constants/categories";
+import { formatRoute } from "utils/formatRoute";
 import styles from "components/Footer/Footer.module.scss";
+
+import TelegramIcon from "public/static/icons/socials/telegram.svg";
+import ViberIcon from "public/static/icons/socials/viber.svg";
+import WhatsAppIcon from "public/static/icons/socials/whatsapp.svg";
+import VkIcon from "public/static/icons/socials/vkontakte.svg";
+import InstagramIcon from "public/static/icons/socials/instagram.svg";
 
 export const Footer: React.FC = () => {
     return (
         <footer className={styles.footer}>
-            <Container>123</Container>
+            <Container>
+                <div className={styles.widgets}>
+                    <div className={styles.contacts}>
+                        <Link href={ERoute.Main}>
+                            <a className={styles.logo}>SAKI.OPT</a>
+                        </Link>
+                        <p
+                            className={clsx(
+                                styles.description,
+                                "paragraph-medium-font"
+                            )}
+                        >
+                            Интернет-магазин Saki.Opt – это доступные цены, широкий,
+                            регулярно обновляемый ассортимент. Покупателям
+                            предлагается электроника, одежда, детские и медицинские
+                            товары.
+                        </p>
+                    </div>
+                    <div className={styles.links}>
+                        <ul>
+                            <li className={styles.link}>
+                                <Link href={ERoute.Login}>
+                                    <a>Войти</a>
+                                </Link>
+                            </li>
+                            <li className={styles.link}>
+                                <Link href={ERoute.About}>
+                                    <a>О нас</a>
+                                </Link>
+                            </li>
+                            <li className={styles.link}>
+                                <Link href={ERoute.Shipping}>
+                                    <a>Доставка</a>
+                                </Link>
+                            </li>
+                            <li className={styles.link}>
+                                <Link href={ERoute.Payment}>
+                                    <a>Оплата</a>
+                                </Link>
+                            </li>
+                        </ul>
+                        <ul>
+                            {CATEGORIES.map(category => (
+                                <li key={category.slug} className={styles.link}>
+                                    <Link
+                                        href={formatRoute(
+                                            ERoute.Category,
+                                            category.slug
+                                        )}
+                                    >
+                                        <a>{category.title}</a>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={styles.newsletter}>
+                        <div className={styles.telegram}>
+                            <p
+                                className={clsx(
+                                    styles.title,
+                                    "paragraph-medium-font"
+                                )}
+                            >
+                                Хотите быть в курсе всех акций и скидок? <br />
+                                Подпишитесь на наш канал в Telegram
+                            </p>
+                            <div>
+                                <TextField
+                                    defaultValue="https://t.me/sakiopt"
+                                    readOnly
+                                />
+                                <Button
+                                    className={styles.subscribe}
+                                    variant="contained"
+                                >
+                                    Подписаться
+                                </Button>
+                            </div>
+                        </div>
+                        <div className={styles.socials}>
+                            <p
+                                className={clsx(
+                                    styles.title,
+                                    "paragraph-medium-font"
+                                )}
+                            >
+                                Присоединяйтесь к нам в соц. сетях
+                            </p>
+                            <div className={styles.list}>
+                                <a
+                                    href="#"
+                                    className={clsx(
+                                        styles.social,
+                                        styles.socialTelegram
+                                    )}
+                                    target="_blank"
+                                >
+                                    <TelegramIcon />
+                                </a>
+                                <a
+                                    href="#"
+                                    className={clsx(
+                                        styles.social,
+                                        styles.socialViber
+                                    )}
+                                    target="_blank"
+                                >
+                                    <ViberIcon />
+                                </a>
+                                <a
+                                    href="#"
+                                    className={clsx(
+                                        styles.social,
+                                        styles.socialWhatsApp
+                                    )}
+                                    target="_blank"
+                                >
+                                    <WhatsAppIcon />
+                                </a>
+                                <a
+                                    href="#"
+                                    className={clsx(styles.social, styles.socialVk)}
+                                    target="_blank"
+                                >
+                                    <VkIcon />
+                                </a>
+                                <a
+                                    href="#"
+                                    className={clsx(
+                                        styles.social,
+                                        styles.socialInstagram
+                                    )}
+                                    target="_blank"
+                                >
+                                    <InstagramIcon />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </footer>
     );
 };
